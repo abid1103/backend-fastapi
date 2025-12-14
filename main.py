7740045ff048c9049f9ba9904187cc18eed32188
Muhammad Abid Hussain <144558738+abid1103@users.noreply.github.com>
2025-12-14 16:37:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -64,11 +64,7 @@ CATEGORY_API_MAPPING = {
 # --- CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:8000",
-    ],
+    allow_origins=[*],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
